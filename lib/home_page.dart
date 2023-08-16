8f580b6310e3aef833aa61629d66f8dea7fc72d2
Soroush Beigi <sbeygi712@gmail.com>
2023-08-16 17:00:40 +0430

    fixing BlocProvider bug

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,46 +12,43 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (BuildContext context) => CounterCubit(),
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              BlocBuilder<CounterCubit, int>(
-                builder: (context, state) => Text(
-                  state.toString(),
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 25,
-                  ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            BlocBuilder<CounterCubit, int>(
+              builder: (context, state) => Text(
+                state.toString(),
+                style: const TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 25,
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () =>
-                        BlocProvider.of<CounterCubit>(context).decrease(),
-                    child: const Text('-'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () =>
-                        BlocProvider.of<CounterCubit>(context).decrease(),
-                    child: const Text('+'),
-                  ),
-                ],
-              ),
-            ],
-          ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () =>
+                      BlocProvider.of<CounterCubit>(context).decrease(),
+                  child: const Text('-'),
+                ),
+                ElevatedButton(
+                  onPressed: () =>
+                      BlocProvider.of<CounterCubit>(context).increase(),
+                  child: const Text('+'),
+                ),
+              ],
+            ),
+          ],
         ),
-        //
       ),
+      //
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:counter_cubit/bloc/counter_cubit.dart';
 import 'package:counter_cubit/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,8 +17,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Counter'),
+      home: BlocProvider(
+        create: (context) => CounterCubit(),
+        child: const MyHomePage(title: 'Counter'),
+      ),
     );
   }
 }
-
